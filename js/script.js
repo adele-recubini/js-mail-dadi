fbd78a4264841ae34736027503fcc77325c9ae82
adele <adele.r87@gmail.com>
2020-12-15 16:13:38 +0100

    dadi

--- js/script.js
+++ js/script.js
@@ -12,17 +12,32 @@ var domandaMail = prompt('qual\ è la tua mail?');
 
 var array = ['adele.r87@gmail.com' , 'anonimo@gmail.com' , 'carlo@gmail.com'];
 
-for(var i = 0; i < array.lenght; i++) {
+for(var i = 0; i < array.length; i++) {
   if( domandaMail === array[i] ){
     output.append('ora puoi giocare');
   } else {
-    alert('error')
+  if( domandaMail !== array[i] ){
+  alert('effettua la registrazione');
+  }
   }
   }
 
+
+//
 // Gioco dei dadi
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-//
-// var myShot = Math.floor(Math.random() * 5) +1;
-// var pcShot = Math.floor(Math.random() * 5) +1;
+
+var min = 1;
+var max = 6;
+
+var playerPc = prompt( Math.floor(Math.random() * (max + 1 - min) + min));
+var player = prompt( Math.floor(Math.random() * (max + 1 - min) + min));
+
+if( playerPc > player) {
+  output.append('hai vinto');
+} else {
+if (playerPc < player) {
+  output.append('hai perso');
+  }
+}
